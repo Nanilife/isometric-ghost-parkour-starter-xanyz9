b4e4d854f9a4ae34ec163fdc368717831088fdc4
Nanilife <88942070+Nanilife@users.noreply.github.com>
2022-11-04 18:02:04 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -13,9 +13,9 @@ export const dy = 10;
 
 const levelString = `
   Snow+None GrEa+None SnSt+None Gren+None Gren+None
-  Gren+None Empt+None Gren+None Gren+None Gren+None
-  Gren+None Gren+None Gren+None Gren+None Gren+None
-  Gren+None Gren+None Gren+None Gren+None Gren+None
+  SnSs+None Empt+None Gren+None Gren+None Gren+None
+  Gren+None SnSs+None SnSs+None Gren+None Gren+None
+  Gren+None Gren+None SnSs+None Gren+None SnSs+None
   Gren+None Gren+None Gren+None Gren+None GrSu+01Pi`;
 const level = parseLevel(levelString);
 
